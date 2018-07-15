0ee1b8f86618ea6fee7cb271a5e6e5c823a2083e
hschweitzer <hadrian54137@gmail.com>
2018-07-16 00:06:13 +0200

    10 pt scores are now highlighted and improved progress visibility

---
+++ contentScript.js
@@ -0,0 +1,23 @@
+let scoreElementArray = document.getElementsByClassName('score')
+let progressArray = document.getElementsByClassName('progress')
+let scoreArray = []
+let scoreElementArraySorted = []
+
+for (let i = 0; i < scoreElementArray.length; i++) {
+    if(scoreElementArray[i].innerText !== "0" && scoreElementArray[i].innerText !== "Score")
+    {
+        scoreArray.push(scoreElementArray[i].innerText)
+        scoreElementArraySorted.push(scoreElementArray[i])
+    }
+}
+
+//TODO get scoring system from popup page and replace this variable accordingly
+let accuracy = 10
+for (let i = 0; i < scoreElementArraySorted.length; i++) {
+    let score = Math.ceil(15 + scoreArray[i] * 80 / accuracy)
+    scoreElementArraySorted[i].style.color = 'hsl('+ score +', 100%, 50%)'
+}
+
+for (let i = 0; i < progressArray.length; i++) {
+    progressArray[i].style.color = 'inherit'
+}
