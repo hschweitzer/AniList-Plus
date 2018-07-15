1b7d5afa976cc32b09253dcda0f923398b2bc419
hschweitzer <hadrian54137@gmail.com>
2018-07-16 00:42:32 +0200

    debugging of contentscript

--- contentScript.js
+++ contentScript.js
@@ -2,7 +2,8 @@ let scoreElementArray = document.getElementsByClassName('score')
 let progressArray = document.getElementsByClassName('progress')
 let scoreArray = []
 let scoreElementArraySorted = []
-
+console.log(scoreElementArray)
+console.log(scoreElementArray[0].innerText)
 for (let i = 0; i < scoreElementArray.length; i++) {
     if(scoreElementArray[i].innerText !== "0" && scoreElementArray[i].innerText !== "Score")
     {
@@ -10,12 +11,13 @@ for (let i = 0; i < scoreElementArray.length; i++) {
         scoreElementArraySorted.push(scoreElementArray[i])
     }
 }
-
+console.log(scoreElementArraySorted)
 //TODO get scoring system from popup page and replace this variable accordingly
 let accuracy = 10
 for (let i = 0; i < scoreElementArraySorted.length; i++) {
     let score = Math.ceil(15 + scoreArray[i] * 80 / accuracy)
     scoreElementArraySorted[i].style.color = 'hsl('+ score +', 100%, 50%)'
+    console.log(score)
 }
 
 for (let i = 0; i < progressArray.length; i++) {
