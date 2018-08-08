97fc5237a3ba8e30eb06362e0d110529535f5d01
hschweitzer <hadrian54137@gmail.com>
2018-08-08 19:18:34 +0200

    proper test of the DAO + API and some reformatting

--- content-DAO.js
+++ content-DAO.js
@@ -1,3 +1,9 @@
+/*
+*
+* CONTENT SCRIPT
+*
+*/
+
 'use strict'
 
 function showStudioLink(navBar) {
@@ -8,22 +14,26 @@ function showStudioLink(navBar) {
 }
 
 var observer = new MutationObserver(function() {
-    // console.log('a mutation occured in the document')
     let navBar = document.querySelector('.content>.nav')
     let url = window.location.href
     let studioLink = document.getElementById('studio_link')
-    if(navBar && !studioLink && (url.includes('/anime/') || url.includes('/manga/'))) {
+    if(navBar && !studioLink && (url.includes('/anime/'))) { // When re-visiting the page of an anime the studio link will sometimes be loaded before the first test
         showStudioLink(navBar)
-        test()
+        mediaTitle(mediaId())
     }
 })
 
-function test() {
+function mediaTitle(a) {
     var variables = {
-        id: 100922
+        id: a
     }
     var dao = new DAO(variables)
     dao.getTitle()
+}
+
+function mediaId() {
+    let url = window.location.href
+    return url.split('/')[4]
 }
 
 observer.observe(document, { childList: true, subtree: true })
@@ -63,22 +73,6 @@ class DAO {
         this.url = 'https://graphql.anilist.co'
     }
 
-    getTitle(mediaId) {
-    
-        var query = `
-        query ($id: Int) {
-            Media (id: $id, type: ANIME) { 
-                id
-                title {
-                romaji
-                english
-                native
-                }
-            }
-        }
-        `
-        this.fetchQuery(query)
-    }
     
     fetchQuery(query) {
         var url = this.url
@@ -93,10 +87,10 @@ class DAO {
                 variables: this.variables
             })
         }
-    
+        
         fetch(url, options).then(this.handleResponse)
-                           .then(this.handleData)
-                           .catch(this.handleError)
+        .then(this.handleData)
+        .catch(this.handleError)
     }
     
     handleResponse(response) {
@@ -113,4 +107,34 @@ class DAO {
     handleData(data) {
         console.log(data);
     }
+
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //  QUERY (see https://anilist.github.io/ApiV2-GraphQL-Docs/query.doc.html)  //
+    ///////////////////////////////////////////////////////////////////////////////
+    
+
+    getTitle(mediaId) {
+    
+        var query = `
+        query ($id: Int) {
+            Media (id: $id, type: ANIME) { 
+                id
+                title {
+                romaji
+                english
+                native
+                }
+            }
+        }
+        `
+        this.fetchQuery(query)
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    //  MUTATION (see https://anilist.github.io/ApiV2-GraphQL-Docs/mutation.doc.html)  //
+    /////////////////////////////////////////////////////////////////////////////////////
+
+
 }
