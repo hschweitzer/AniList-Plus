dbd15d66924084cfca74cbfdc32a9444305fb86f
hschweitzer <hadrian54137@gmail.com>
2018-10-11 21:51:42 +0200

    style for studio cards is now manually applied because I can't be bothered to learn how vue.js works

--- content-DAO.js
+++ content-DAO.js
@@ -73,9 +73,102 @@ function mediaId() {
 }
 
 function createStudiosElement(studioList) {
+    const styleElement = document.createElement('style')
+    const style = `
+    .studio-card {
+        margin-bottom: 15px;
+        margin-right: 20px;
+        position: relative;
+        width: 85px;
+        background: rgb(var(--color-foreground));
+        border-radius: 3px;
+        display: inline-grid;
+        grid-template-columns: 85px auto;
+        height: 115px;
+        max-height: 115px;
+    }
+    
+    .studio-card .cover {
+        border-radius: 3px;
+        text-align: center;
+        background-position: 50%;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    
+    .studio-card .cover:hover {
+        border-radius: 3px 0 0 3px;
+    }
+    
+    .studio-card .cover .image-text {
+        background: rgba(var(--color-overlay),.7);
+        border-radius: 0 0 3px 3px;
+        bottom: 0;
+        color: rgba(var(--color-text-bright),.91);
+        display: inline-block;
+        font-size: 1.2rem;
+        font-weight: 400;
+        left: 0;
+        letter-spacing: .2px;
+        margin-bottom: 0;
+        padding-bottom: 10px;
+        padding-top: 10px;
+        position: absolute;
+        transition: .3s;
+        width: 100%;
+        text-align: center;
+    }
+    
+    .studio-card .cover:hover .image-text {
+        opacity: 0;
+    }
+    
+    .studio-card .cover+.content {
+        height: 100%;
+        left: 100%;
+        position: absolute;
+        top: 0;
+    }
+    
+    .studio-card .content {
+        opacity: 0;
+        transition: opacity .3s;
+        width: 240px;
+        z-index: -1;
+        background: rgb(var(--color-foreground));
+        border-radius: 0 3px 3px 0;
+        padding: 12px;
+        position: relative;
+    }
+    
+    .studio-card .info-header {
+        color: rgb(var(--color-blue));
+        font-size: 1.2rem;
+        font-weight: 500;
+        margin-bottom: 8px;
+    }
+    
+    .studio-card .title {
+        font-size: 1.4rem;
+    }
+    
+    .studio-card .info {
+        bottom: 12px;
+        color: rgb(var(--color-text-lighter));
+        font-size: 1.2rem;
+        left: 12px;
+        position: absolute;
+    }
+    
+    .studio-card .cover:hover+.content {
+        display: block;
+        opacity: 1;
+        z-index: 9;
+    }
+    `
+    styleElement.innerHTML = style.trim();
     let studiosElement = document.createElement('div')
     studiosElement.id = "studios"
-    
     for (var i = 0; i < studioList.length; i++) {
         const mediaList = studioList[i].media.nodes
         let studio = document.createElement('div')
@@ -84,13 +177,11 @@ function createStudiosElement(studioList) {
         studio.appendChild(studioName)
         for (var j = 0; j < mediaList.length; j++) {
             let mediaElement = document.createElement('div')
-            mediaElement.classList.add("media-preview-card", "small")
+            mediaElement.classList.add("media-preview-card", "small", "studio-card")
             /**
              * TODO IMPORTANT
              * data-v-xxxxxxxx needs to be fetched in some way or the css needs to be injected manually QUICKLY
              */
-            mediaElement.setAttribute("data-v-2737c4f7", "")
-            mediaElement.setAttribute("data-v-9c15f6ba", "")
             const mediaLink = "/anime/" + mediaList[j].id
             const mediaCover = mediaList[j].coverImage.medium
             const mediaTitle = mediaList[j].title.userPreferred
@@ -99,17 +190,17 @@ function createStudiosElement(studioList) {
             const mediaScore = mediaList[j].averageScore
             const mediaYear = mediaList[j].startDate.year
             const mediaString =
-            `<a data-v-2737c4f7 href="${mediaLink}" class="cover" data-src="${mediaCover}" lazy="loaded" style="background-image: url(&quot;${mediaCover}&quot;);">
-                <div data-v-2737c4f7 class="image-text">
+            `<a href="${mediaLink}" class="cover" data-src="${mediaCover}" lazy="loaded" style="background-image: url(&quot;${mediaCover}&quot;);">
+                <div class="image-text">
                     <div data-v-4fd869dd>${mediaYear} · ${mediaScore}</div>
                 </div> <!---->
             </a>
-            <div data-v-2737c4f7 class="content">
-                <div data-v-2737c4f7 class="info-header">
+            <div class="content">
+                <div class="info-header">
                     <!-- <div data-v-9c15f6ba="" data-v-2737c4f7>Side Story</div> -->
                 </div> 
-                <a data-v-2737c4f7 href="${mediaLink}" class="title">${mediaTitle}</a>
-                <div data-v-2737c4f7 class="info">
+                <a href="${mediaLink}" class="title">${mediaTitle}</a>
+                <div class="info">
                 ${mediaFormat} · ${mediaStatus}
                 </div>
             </div>`
@@ -117,6 +208,7 @@ function createStudiosElement(studioList) {
             mediaElement.innerHTML = mediaString.trim()
             studio.appendChild(mediaElement)
         }
+        studiosElement.appendChild(styleElement)
         studiosElement.appendChild(studio)
     }
     return studiosElement
